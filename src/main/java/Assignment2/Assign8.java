73f46322acf0c5131a2ecb0034b9caaf02541438
priyankagitkit <priyanka.test05@gmail.com>
2022-10-11 20:06:58 +0530

    Priyanka Assignment files

---
+++ src/main/java/Assignment2/Assign8.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+import java.util.Scanner;
+
+public class Assign8 {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("enter the string");
+       String name= s.next();
+       System.out.println("Length of string is " +name.length());
+    }
+}
---
+++ src/main/java/Assignment2/Assign9.java
@@ -0,0 +1,9 @@
+package Assignment2;
+
+public class Assign9 {
+    public static void main(String[] args) {
+        String org="The quick brown fox jumps over the lazy dog";
+        System.out.println("The original String is: "+org);
+       System.out.println("The new String is: "+ org.replace("d","f"));
+    }
+}
